8a0ecbaca63b3af1c4ce22590f93746c78c313f5
Sogiefa-12 <131720010+Sogiefa-12@users.noreply.github.com>
2024-11-18 17:47:23 +0100

    commit

--- src/js/product.js
+++ src/js/product.js
@@ -1,29 +1,39 @@
+// import { setLocalStorage } from './utils.mjs';
+// import { getLocalStorage } from './utils.mjs';
+// import ProductData from './ProductData.mjs';
+
+// const dataSource = new ProductData('tents');
+
+// function addProductToCart(product) {
+//   let cartItems = getLocalStorage('so-cart');
+
+//   if (!cartItems) {
+//     cartItems = [];
+//   } else if (!Array.isArray(cartItems)) {
+//     // Handle case where previous item was stored as a single object
+//     cartItems = [cartItems];
+//   }
+
+//   cartItems.push(product);
+//   setLocalStorage('so-cart', cartItems);
+// }
+// // add to cart button event handler
+// async function addToCartHandler(e) {
+//   const product = await dataSource.findProductById(e.target.dataset.id);
+//   addProductToCart(product);
+// }
+
+// // add listener to Add to Cart buttonnpm
+// document
+//   .getElementById('addToCart')
+//   .addEventListener('click', addToCartHandler);
+
 import { setLocalStorage } from './utils.mjs';
 import { getLocalStorage } from './utils.mjs';
+import {getParams}  from './utils.mjs';
 import ProductData from './ProductData.mjs';
-
+import ProductDetails from './ProductDetails.mjs';
+const productId = getParams('product');
 const dataSource = new ProductData('tents');
-
-function addProductToCart(product) {
-  let cartItems = getLocalStorage('so-cart');
-
-  if (!cartItems) {
-    cartItems = [];
-  } else if (!Array.isArray(cartItems)) {
-    // Handle case where previous item was stored as a single object
-    cartItems = [cartItems];
-  }
-
-  cartItems.push(product);
-  setLocalStorage('so-cart', cartItems);
-}
-// add to cart button event handler
-async function addToCartHandler(e) {
-  const product = await dataSource.findProductById(e.target.dataset.id);
-  addProductToCart(product);
-}
-
-// add listener to Add to Cart buttonnpm
-document
-  .getElementById('addToCart')
-  .addEventListener('click', addToCartHandler);
+const product = new ProductDetails(productId, dataSource);
+product.init();
--- src/js/utils.mjs
+++ src/js/utils.mjs
@@ -13,6 +13,42 @@ export function getLocalStorage(key) {
 export function setLocalStorage(key, data) {
   localStorage.setItem(key, JSON.stringify(data));
 }
+// // set a listener for both touchend and click
+// export function setClick(selector, callback) {
+//   qs(selector).addEventListener("touchend", (event) => {
+//     event.preventDefault();
+//     callback();
+//   });
+//   qs(selector).addEventListener("click", callback);
+// }
+
+
+export function getParams(param){
+  const queryString = window.location.search;
+  const urlParams = new URLSearchParams(queryString);
+  const value  = urlParams.get(param);
+  return value;
+
+}
+
+// Copied from Instructor
+// function to take a list of objects and a template and insert the objects as HTML into the DOM
+
+export function renderListWithTemplate(
+  templateFn,
+  parentElement,
+  list,
+  position = "afterbegin",
+  clear = false
+) {
+  const htmlStrings = list.map(templateFn);
+  // if clear is true we need to clear out the contents of the parent.
+  if (clear) {
+    parentElement.innerHTML = "";
+  }
+  parentElement.insertAdjacentHTML(position, htmlStrings.join(""));
+}
+
 // set a listener for both touchend and click
 export function setClick(selector, callback) {
   qs(selector).addEventListener("touchend", (event) => {
